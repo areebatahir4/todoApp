b4eada8124ecbe5a85a112ea413788e008c86b4c
areebatahir4 <areebatahir4@gmail.com>
2020-07-27 10:45:03 +0500

    changes in edit button

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
+var list = document.getElementById("list");
+function addTodo() {
+    var todo_item = document.getElementById("todo-item");
+    var li = document.createElement('li');
+    var liText = document.createTextNode(todo_item.value);
+    li.appendChild(liText);
+    //list.appendChild(li);
 
-var list= document.getElementById("list");
-function addTodo(){
-    var todo_item= document.getElementById("todo-item")
-    var li = document.createElement('li')
-    var liText = document.createTextNode (todo_item.value)
-    li.appendChild(liText)
-    list.appendChild(li)
+    var editBtn = document.createElement("button");
+    var editText = document.createTextNode("EDIT");
+    editBtn.setAttribute("Class", "btn");
+    editBtn.appendChild(editText);
+    editBtn.setAttribute("onclick", "editItem(this)");
+
+    var delBtn = document.createElement("button");
+    var delText = document.createTextNode("DELETE");
+    delBtn.appendChild(delText);
+    li.appendChild(delBtn);
+    delBtn.setAttribute("Class", "btn");
+    delBtn.setAttribute("onclick", "deleteItem(this)");
+    delBtn.appendChild(delText);
+    li.appendChild(delBtn);
+    li.appendChild(editBtn);
+    list.appendChild(li);
 
 
-    var delBtn=document.createElement ("button")
-    var delText = document.createTextNode ("DELETE")
-    delBtn.appendChild (delText)
-    li.appendChild (delBtn)
-    delBtn.setAttribute ("Class", "btn")
-    delBtn.setAttribute ("onclick", "deleteItem(this)")
-    delBtn.appendChild(delText)
-    li.appendChild(delBtn)
-    li.appendChild(editBtn)
-    list.appendChild(li)
+    todo_item.value = "";
+    console.log(li);
 
-
-    todo_item.value =""
-    console.log     (li)
-   
 }
-function deleteItem(e){
-    e.parentNode.remove ()
-    console.log (e)
+function deleteItem(e) {
+    e.parentNode.remove();
+    console.log(e);
 }
-function deleteAll(){
-    list.innerHTML = ""
+function deleteAll() {
+    list.innerHTML = "";
 }
-var editBtn = document.createElement("button");
-var editText =document.createTextNode ("EDIT")
-editBtn.setAttribute ("Class", "btn")
-editBtn.appendChild(editText)
-
-editBtn.setAttribute ("onclick","editItem(this)")
 
 
-function editItem(e){
 
-var val= prompt ("Enter updated value",e.parentNode.firstChild.nodeValue)
-e.parentNode.firstChild.nodeValue= val;
+function editItem(e) {
+
+    var val = prompt("Enter updated value", e.parentNode.firstChild.nodeValue);
+    e.parentNode.firstChild.nodeValue = val;
 
 
 }
